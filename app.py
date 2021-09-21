87777e36bb82c7b785e297124aa60bf7a952435c
Zabir Anik <shahariar.arpo@gmail.com>
2021-09-21 13:52:57 +0600

    update: block placement

--- app.py
+++ app.py
@@ -233,8 +233,19 @@ def main():
     next_piece = get_shape()
     clock = pygame.time.Clock()
     fall_time = 0
+    fall_speed = 0.27
 
     while run:
+        grid = create_grid(locked_positions)
+        fall_time += clock.get_rawtime()
+        clock.tick()
+        if fall_time / 100 > fall_speed:
+            fall_time = 0
+            current_piece.y += 1
+            if not  valid_space(current_piece, grid) and current_piece.y > 0:
+                current_piece.y -= 1
+                change_piece = True
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
